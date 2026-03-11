50217d0c67b38a14a889e10abc091303b5edc437
gaurav <gaurav15199singh@gmail.com>
2026-03-11 22:26:10 +0530

    improve scheduled notification validation to run only when scheduled

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,16 +3,16 @@ class Notification < ApplicationRecord
 
   validates :title, presence: true
   validates :message, presence: true
+  validates :scheduled_at, presence: true, if: :scheduled?
 
   belongs_to :creator, class_name: "User", foreign_key: "created_by"
+
   has_many :user_notifications
   has_many :users, through: :user_notifications
 
-  validate :scheduled_time_in_future
+  validate :scheduled_time_in_future, if: :scheduled?
 
   def scheduled_time_in_future
-    return if scheduled_at.blank?
-
     if scheduled_at <= Time.current
       errors.add(:scheduled_at, "must be in the future")
     end
